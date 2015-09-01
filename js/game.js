050f09c1439c8556974e5e65b943d46f126733d1
lc1915 <liuchang1915@gmail.com>
2015-09-01 13:20:45 +0800

    modify details

--- js/game.js
+++ js/game.js
@@ -103,7 +103,7 @@ var render = function () {
 	ctx.font = "24px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
-	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+	ctx.fillText("抓住的617数量！: " + monstersCaught, 32, 32);
 };
 
 // The main game loop
